0cc25ea940ab3977bfa07935021ce8d897fac4ec
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-08-14 20:45:17 -0300

    Connected application to websocket

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import { REACT_APP_WS_URL } from "@env";
+
+const App = () => {
+  const [sucessfulConnection, setSucessfulConnection] = useState(false);
+  const [socketLastMessage, setSocketLastMessage] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+
+  const createConnection = () => {
+    try {
+      const ws = new WebSocket(REACT_APP_WS_URL);
+      ws.onopen = () => {
+        console.log("Connected to socket: %s", REACT_APP_WS_URL);
+        setErrorMessage(undefined);
+        setSucessfulConnection(true);
+      };
+      ws.onmessage = ({ data }) => {
+        setSocketLastMessage(data);
+      };
+      ws.onclose = () => {
+        setSucessfulConnection(false);
+        setErrorMessage("Closed communication");
+      };
+      ws.onerror = ({ message }) => {
+        setSucessfulConnection(false);
+        setErrorMessage(message);
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View
+      style={
+        StyleSheet.create({
+          container: {
+            padding: 30,
+          },
+        }).container
+      }
+    >
+      <Text>Testing WebSocket connection</Text>
+      {sucessfulConnection ? (
+        <View>
+          <Text>Connected to Websocket</Text>
+          {socketLastMessage && <Text>{socketLastMessage}</Text>}
+        </View>
+      ) : (
+        <Text>Not available connection</Text>
+      )}
+      {errorMessage && <Text>{errorMessage}</Text>}
+      <Button onPress={createConnection} title="Test Connection" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
